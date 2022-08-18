52c9fbc7a3ab7240ecda194ee816faa273d9e3d0
Mohammed <mhmdtajeldin@gmail.com>
2022-08-18 12:01:11 +0200

    Reusing multiple times

--- src/App.js
+++ src/App.js
@@ -32,22 +32,39 @@ function App() {
           type = "text" / >
 
           <hr / >
-        {
-        list.map(function(item) {
-                return ( 
-                  <div key = { item.objectID } >
-                    <span>
-                    <a href = { item.url } > { item.title } </a> </span> 
-                    <span > { item.author } </span> 
-                    <span > { item.num_comments } </span>
-                    <span > { item.points } </span> 
-                    </div>
-                );
-            })
-        }
+
+          <List />
+          
+          <List />
+
+          <List />
+          
+          <List />
+          
+          
+          
+
+
 
         </div>
     );
 }
+
+
+function List() {
+return list.map(function(item) {
+return (
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </div>
+      );
+    });
+}
+
 
 export default App;
